0a34fe316aaaacfb7a59ae120dbb2712ee722357
Mohamed Arif <3gwebtrain@gmail.com>
2018-03-12 08:16:13 +0530

    book details component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BookDetailsComponent} from './book-details/book-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule
