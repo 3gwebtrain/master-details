168a0c00ccf134893eaa917539652b8318cf2cfa
Mohamed Arif <3gwebtrain@gmail.com>
2018-03-20 08:33:54 +0530

    registration reactive form added

--- src/app/registration-reactive-form/registration-reactive-form.component.ts
+++ src/app/registration-reactive-form/registration-reactive-form.component.ts
@@ -6,4 +6,23 @@ import {FormGroup, FormControl,Validators} from "@angular/forms";
 	templateUrl:"./registration-reactive-form.component.html"
 })
 
-export class RegistrationReactiveFormComponent {}
+export class RegistrationReactiveFormComponent implements OnInit {
+
+	EMAIL_REGEX = "[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*";
+	registrationForm:FormGroup;
+
+	ngOnInit(){
+		this.registrationForm = new FormGroup({
+			firstName: new FormControl('Shravan', Validators.required),
+			lastName: new FormControl(''),
+			email: new FormControl('', [Validators.required,Validators.pattern(this.EMAIL_REGEX)])
+		})
+	}
+
+	onSubmit(formValue) {
+		console.log(formValue);
+		console.log(this.registrationForm.value)
+	}
+
+
+}
